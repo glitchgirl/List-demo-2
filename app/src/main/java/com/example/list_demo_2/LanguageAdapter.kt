50ca6d5c5da3aaff83f7384392a38f77184b9750
Morgan Smith <londonspleen@gmail.com>
2026-02-25 10:27:50 -0500

    add list

---
+++ app/src/main/java/com/example/list_demo_2/LanguageAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.list_demo_2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class LanguageAdapter(private val languages: List<String>) :
+    RecyclerView.Adapter<LanguageAdapter.LanguageViewHolder>() {
+
+    class LanguageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val txtLanguage: TextView = itemView.findViewById(R.id.txtLanguage)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LanguageViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_language, parent, false)
+        return LanguageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: LanguageViewHolder, position: Int) {
+        holder.txtLanguage.text = languages[position]
+    }
+
+    override fun getItemCount(): Int {
+        return languages.size
+    }
+}
+
